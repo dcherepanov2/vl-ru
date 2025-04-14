22e2e58778af08ac10aa44f9b7874e6baa2d9308
Danil <danilcherep7@gmail.com>
2025-04-14 23:11:04 +0700

    feat():mvp

--- src/main/java/service/processor/DefaultLogProcessor.java
+++ src/main/java/service/processor/DefaultLogProcessor.java
@@ -1,6 +1,5 @@
 package service.processor;
 
-import com.google.common.base.Stopwatch;
 import config.LineProcessorConfig;
 import domain.InputUserData;
 import domain.LogBatchProcessData;
@@ -8,8 +7,6 @@ import service.helper.DefaultResourceCalculator;
 import service.helper.ResourceCalculator;
 import service.parser.DefaultInputUserParser;
 import service.parser.InputUserParser;
-
-import java.util.List;
 import java.util.concurrent.*;
 
 public class DefaultLogProcessor implements LogProcessor {
@@ -18,63 +15,30 @@ public class DefaultLogProcessor implements LogProcessor {
     private final BlockingQueue<String> lineConsoleBlockingQueue;
     private final BlockingQueue<LogBatchProcessData> logBatchProcessDataBlockingQueue;
     private final InputUserParser inputUserParser;
-    private final Stopwatch stopwatch;
 
     public DefaultLogProcessor(LineProcessorConfig lineProcessorConfig) {
         this.lineProcessorConfig = lineProcessorConfig;
         this.logBatchProcessDataBlockingQueue = new LinkedBlockingQueue<>();
         this.lineConsoleBlockingQueue = new LinkedBlockingQueue<>();
         this.inputUserParser = new DefaultInputUserParser();
-        this.stopwatch = Stopwatch.createUnstarted();
     }
 
     @Override
     public void start(String[] args) {
         ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
         InputUserData inputUserData = inputUserParser.parse(args);
-        stopwatch.start();
 
-        List<CompletableFuture<?>> workers = createWorkers(executorService, inputUserData);
-        boolean shouldContinue = true;
-
-        while (shouldContinue && !executorService.isShutdown()) {
-            shouldContinue = checkWorkersStatus(workers);
-        }
+        CompletableFuture<Void> workers = createWorkers(executorService, inputUserData);
+        workers.join();
 
         executorService.shutdownNow();
     }
 
-    private boolean checkWorkersStatus(List<CompletableFuture<?>> workers) {
-        if (!isTimeForWorkersChecker()) {
-            return true;
-        }
-
-        boolean hasExceptions = workers.stream()
-                .anyMatch(CompletableFuture::isCompletedExceptionally);
-
-        return !hasExceptions;
-    }
-
-    private boolean isTimeForWorkersChecker(){
-        long elapsed = stopwatch.elapsed(TimeUnit.SECONDS);
-        if(elapsed > 5){
-            stopwatch.reset().start();
-            return true;
-        }
-        return false;
-    }
-
-    private List<CompletableFuture<?>> createWorkers(ExecutorService executorService, InputUserData inputUserData) {
-        CompletableFuture<Void> consoleWriteProcessor =
-                CompletableFuture.runAsync(() -> configureConsoleWriteProcessor(inputUserData).run(), executorService);
-        CompletableFuture<Void> lineBatchProcessor =
-                CompletableFuture.runAsync(() -> configureLineParseProcessor(inputUserData).run(), executorService);
-        CompletableFuture<Void> consoleInputReadProcessor =
-                CompletableFuture.runAsync(() -> configureConsoleInputReadProcessor().run(), executorService);
-        return List.of(
-                consoleWriteProcessor,
-                consoleInputReadProcessor,
-                lineBatchProcessor
+    private CompletableFuture<Void> createWorkers(ExecutorService executorService, InputUserData inputUserData) {
+        return CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> configureConsoleWriteProcessor(inputUserData).run(), executorService),
+                CompletableFuture.runAsync(() -> configureConsoleInputReadProcessor().run(), executorService),
+                CompletableFuture.runAsync(() -> configureLineParseProcessor(inputUserData).run(), executorService)
         );
     }
 
